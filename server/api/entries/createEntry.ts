a3b8e89cea4a7b4e7156d9f3f59cfba8dca24bd2
Jana <57497690+codeblonde@users.noreply.github.com>
2025-02-17 11:11:21 +0100

    added analysis db insert

--- server/api/entries/createEntry.ts
+++ server/api/entries/createEntry.ts
@@ -1,6 +1,6 @@
 import { analyzeEntry } from "~/server/ai/getAnalysis"
 import { db } from "~/server/db/client"
-import { journalEntries } from "~/server/db/schema"
+import { journalEntries, analysis } from "~/server/db/schema"
 
 export default defineEventHandler(async (event) => {
     const entry = await readBody(event)
@@ -9,9 +9,23 @@ export default defineEventHandler(async (event) => {
         insertedId: journalEntries.entryId
     })
 
-    const analysis = await analyzeEntry(entry.data)
-    console.log(analysis)
 
+    if (!dbResult[0]?.insertedId) {
+        throw new Error("Failed to insert journal entry");
+    }
+
+    const analysisAI = await analyzeEntry(entry.data)
+    console.log(analysisAI)
+
+    const analysisResult = await db.insert(analysis).values({ 
+        analysisId: dbResult[0].insertedId,
+        mood: analysisAI.mood,
+        color: analysisAI.color,
+        score: analysisAI.score,
+        summary: analysisAI.summary,
+        suggestion: analysisAI.suggestion,
+    
+        })
 
     console.log(dbResult)
     return dbResult[0]
--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,5 +1,5 @@
 import { createId } from "@paralleldrive/cuid2";
-import { sql } from "drizzle-orm";
+import { sql, relations } from "drizzle-orm";
 import { datetime, timestamp } from "drizzle-orm/mysql-core"
 import { blob, int, sqliteTable, text } from "drizzle-orm/sqlite-core"
 
@@ -8,12 +8,20 @@ export const journalEntries = sqliteTable("journal_entries",
   entryId: text("entryId").primaryKey().$defaultFn(() => createId()),
   createdAt: text("createdAt").default(sql`(CURRENT_TIMESTAMP)`),
   entryText: text("entryText").notNull(),
+  // optional analysis link:
+  // analysisId: text("analysisId").unique().references(() => analysis.analysisId, { onDelete: "cascade" })
 });
 
-const analysis = sqliteTable("analysis", 
+
+
+export const analysis = sqliteTable("analysis", 
   {
-  analysisId: text().primaryKey().$defaultFn(() => createId()),
-  createdAt: text().default(sql`(CURRENT_TIMESTAMP)`),
+  // analysisId: text().primaryKey().$defaultFn(() => createId()),
+  analysisId: text("analysisId")
+    .primaryKey()
+    .references(() => journalEntries.entryId, { onDelete: "cascade" }), 
+  // journalId: text('entryId').references(() => journalEntries.entryId),
+  // createdAt: text().default(sql`(CURRENT_TIMESTAMP)`),
   mood: text().notNull(),
   score: int().notNull(),
   color: text().notNull(),
@@ -21,3 +29,18 @@ const analysis = sqliteTable("analysis",
   summary: text().notNull()
   
 });
+
+
+export const journalRelations = relations(journalEntries, ({ one }) => ({
+	analysis: one(analysis, {
+    fields: [journalEntries.entryId],
+    references: [analysis.analysisId],
+  }),
+}));
+
+export const analysisRelations = relations(analysis, ({ one }) => ({
+	entry: one(journalEntries, { 
+    fields: [analysis.analysisId], 
+    references: [journalEntries.entryId] 
+  }),
+}));
